b0c0023dc581427893690b026bb99b99df173100
favourwright <gismozfexie@yahoo.com>
2022-03-30 23:32:13 +0100

    Right solution for excercise 10

--- exercise10.js
+++ exercise10.js
@@ -1,1 +1,11 @@
-// 10) Write a JavaScript program to find the number of even digits in a given integer.
+// a function to find the number of even digits in a given integer.
+function countEvenDigits(a){
+    var count = 0;
+    var num = a.toString();
+    for(var i = 0; i < num.length; i++){
+        if(num[i] % 2 === 0){
+            count++;
+        }
+    }
+    return count;
+}
