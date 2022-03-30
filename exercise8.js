de8d90d3dadcb2ea91508ea26ca2deec9fecb0a7
favourwright <gismozfexie@yahoo.com>
2022-03-30 23:16:14 +0100

    Right solution for excercise 8

--- exercise8.js
+++ exercise8.js
@@ -1,19 +1,15 @@
-function studentGrade(a){
-    var grade=a;
-    var finalExam
-    if (a>=89) {
-        console.log(true)
-    }else{console.log(false)}
-}
-studentGrade(50)
-// function exam_status(totmarks,is_exam)
-//   {
-//   if (is_exam) {
-//     return totmarks >= 90;
-//   }
-//  return (totmarks >= 89 && totmarks <= 100);
-//  }
+// The student will get A+ grade if the total marks are in the range 89..100 inclusive,
+// if the examination is "Final-exam." the student will get A+ grade and total marks must be greater than or equal to 90.
+// Return true if the student get A+ grade or false otherwise. 
 
-// console.log(exam_status("78", " "));
-// console.log(exam_status("89", "true "));
-// console.log(exam_status("99", "true "));
+function studentGrade(a, finalExam=false){
+    let grade=a;
+    if (a>=90 && finalExam==true) {
+       return true
+    } else if (a>=89 && finalExam==false) {
+        return true
+    } else {
+       return false
+    }
+}
+console.log(studentGrade(100, true))
