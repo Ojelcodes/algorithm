24a41d2900af8d46a8d6641d933db3b9ca57f6b7
favourwright <gismozfexie@yahoo.com>
2022-03-30 23:30:45 +0100

    Right solution for excercise 9

---
+++ exercise10.js
@@ -0,0 +1,1 @@
+// 10) Write a JavaScript program to find the number of even digits in a given integer.
--- exercise9.js
+++ exercise9.js
@@ -1,13 +1,11 @@
-function lentVowels(a){
-    var a = ""
-    var vowels = "a,e,i,o,u"
-    var count
-    for (let index = a; index < array.length; index++) {
-        const element = array[index];
-        if (element!==vowels) {
-            continue
+// a function to count the number of vowels in a given string.
+function countVowels(a){
+    var vowels = ['a','e','i','o','u'];
+    var count = 0;
+    for(var i = 0; i < a.length; i++){
+        if(vowels.indexOf(a[i]) !== -1){
+            count++;
         }
-        
     }
-    
+    return count;
 }
